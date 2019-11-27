ae34dd545020caa2ac7197c69183e8d201838b98
Michael Adaixo <michael.adaixo@gmail.com>
2019-11-27 23:24:04 +0000

    First version of a file watcher.

---
+++ Core/Assets/FileWatcher.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <chrono>
+
+// Until Cpp2a is not here.
+#include <sys/types.h>
+#include <sys/stat.h>
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#ifdef WIN32
+#define stat _stat
+#endif
+//~ 
+
+#include "AssetManager.h"
+#include "Utils.h"
+
+// Temp
+struct FileEventHandlers
+{
+	std::function<void(void)> OnStatusChanged;
+	std::function<void(void)> OnFileModified;
+	std::function<void(void)> OnFileSaved;
+}; 
+
+enum class FileType : int
+{
+	None = 0,
+	// Shaders
+	Shader_Vert,
+	Shader_Frag,
+	Shader_Geom,
+
+	Count
+};
+
+struct FileTypeInfo
+{
+	std::string m_extension;
+	std::function<void(const std::string&)> OnFileChanged;
+};
+
+class FileWatcher
+{
+public:
+	FileWatcher() = default;
+
+	FileWatcher(const std::string& rootFolder, float updateDelay) 
+		: m_rootFolder(rootFolder)
+		, m_updateDelay(updateDelay)
+	{ }
+
+	void RegisterFileType(FileType fileType, const std::string& extension, std::function<void(const std::string&)> OnFileChanged)
+	{
+		m_watchedFilesTypes.emplace(fileType, FileTypeInfo({ extension, OnFileChanged }));
+	}
+
+	void Update(float deltaTime)
+	{
+		m_currentTime += deltaTime;
+		if (m_currentTime > m_nextAccessTime)
+		{
+			m_lastAccessTime = m_nextAccessTime;
+			m_nextAccessTime = m_lastAccessTime + m_updateDelay;
+
+			AnalizeFoldersAndFiles();
+		}
+	}
+
+	void AnalizeFoldersAndFiles()
+	{
+		for (auto& pathIt : std::filesystem::recursive_directory_iterator(m_rootFolder))
+		{
+			const std::string pathString = pathIt.path().string();
+			const std::string extension = pathIt.path().extension().string();
+			for (const std::pair<FileType, FileTypeInfo>& info : m_watchedFilesTypes)
+			{
+				if (info.second.m_extension != extension)
+				{
+					continue;
+				}
+
+				// std::chrono::system_clock time = std::filesystem::last_write_time(pathIt);
+				long long time = GetLastModifiedTime(pathString);
+				
+				auto findIt = m_lastEditTime.find(pathString);
+				if (findIt != m_lastEditTime.end())
+				{
+					if (time > findIt->second)
+					{
+						findIt->second = time;
+						info.second.OnFileChanged( Utils::NormalizePath( pathString ) );
+					}
+				}
+				else
+				{
+					m_lastEditTime.emplace(pathString, time);
+				}
+			}
+		}
+	}
+
+	long long GetLastModifiedTime(const std::string& path)
+	{
+		struct stat result;
+		if (stat(path.c_str(), &result))
+		{
+			// Error
+			return 0;
+		}
+		return result.st_mtime;
+	}
+
+	
+
+private:
+	std::unordered_map<FileType, FileTypeInfo> m_watchedFilesTypes;
+	std::unordered_map<FileType, std::vector<std::string>> m_watchedFilesPaths;
+	std::unordered_map<std::string, long long> m_lastEditTime;
+
+	float m_currentTime = 0.0f;
+	float m_lastAccessTime = 0.0f;
+	float m_nextAccessTime = 0.0f;
+	float m_updateDelay = 0.0f;
+
+	std::filesystem::path m_rootFolder;
+};
